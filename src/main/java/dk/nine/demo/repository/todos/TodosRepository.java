d38c13a948b805f14be3030c4248ad99b2949b9e
Jonathan Szigethy <jsz@nine.dk>
2024-10-15 13:18:11 +0200

    changed getTodoLists by search query to List

--- src/main/java/dk/nine/demo/repository/todos/TodosRepository.java
+++ src/main/java/dk/nine/demo/repository/todos/TodosRepository.java
@@ -2,11 +2,11 @@ package dk.nine.demo.repository.todos;
 
 import dk.nine.demo.model.Todos;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface TodosRepository extends JpaRepository<Todos, UUID> {
-    Optional<Todos> findByTitleContainsIgnoreCaseOrderByCreatedAt(String title);
+import java.util.Optional;
+    List<Todos> findByTitleContainsIgnoreCaseOrderByCreatedAt(String title);
 
 }
